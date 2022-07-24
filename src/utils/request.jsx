73df0b4d2558f378e106810ca17d3aea17d8b2f0
Lohan Amendola <lohanamendola18@gmail.com>
2022-07-24 10:37:45 -0400

    add Environment Variables

--- src/utils/request.jsx
+++ src/utils/request.jsx
@@ -1,6 +1,6 @@
 
 // process.env.REACT_APP_BACKEND_URL ?? 
 //"https://shrouded-plateau-27488.herokuapp.com/api"
-const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080/api";
+const BASE_URL = "https://shrouded-plateau-27488.herokuapp.com/api";
 
 export default BASE_URL
