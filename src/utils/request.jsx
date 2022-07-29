5bea33ffea2b8ed0eeef6cc4ae8063f51853d655
Lohan Amendola <lohanamendola18@gmail.com>
2022-07-28 20:55:55 -0400

    commit

--- src/utils/request.jsx
+++ src/utils/request.jsx
@@ -6,6 +6,6 @@
 //"https://api-afastamento.herokuapp.com"
 //import.meta.env.REACT_APP_BACKEND_URL
 
-const BASE_URL = import.meta.env.REACT_APP_BACKEND_URL ?? "https://api-afastamento.herokuapp.com"
+const BASE_URL = import.meta.env.REACT_APP_BACKEND_URL ?? "http://localhost:3333"
 
 export default BASE_URL
