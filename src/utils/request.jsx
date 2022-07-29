2258ad73e8030841f1463b942013efc36ec3975d
Lohan Amendola <lohanamendola18@gmail.com>
2022-07-28 20:44:27 -0400

    commit

--- src/utils/request.jsx
+++ src/utils/request.jsx
@@ -1,9 +1,10 @@
-
+import process from "process";
 // process.env.REACT_APP_BACKEND_URL ?? 
 //"http://localhost:3333/afastamentos"
 
 //"https://shrouded-plateau-27488.herokuapp.com/api"
 //"https://api-afastamento.herokuapp.com"
-const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "https://api-afastamento.herokuapp.com"
+
+const BASE_URL = process.env.REACT_APP_BACKEND_URL
 
 export default BASE_URL
