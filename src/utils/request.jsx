44282161c548cad73a5d1ac279037a2f08f0c481
Lohan Amendola <lohanamendola18@gmail.com>
2022-07-28 20:20:23 -0400

    commit

--- src/utils/request.jsx
+++ src/utils/request.jsx
@@ -4,6 +4,6 @@
 
 //"https://shrouded-plateau-27488.herokuapp.com/api"
 //"https://api-afastamento.herokuapp.com"
-const BASE_URL = REACT_APP_BACKEND_URL
+const BASE_URL = process.env.REACT_APP_BACKEND_URL
 
 export default BASE_URL
