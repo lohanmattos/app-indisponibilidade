b4b60b850aa0d25a0bdc41b639100c425f6cc4f1
Lohan Amendola <lohanamendola18@gmail.com>
2022-08-04 21:19:25 -0400

    adicionando icons de rede social

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./styless.css";
 
 import logo from "/src/assets/pineapple.png"
@@ -11,7 +12,11 @@ function Footer() {
                     <img src={logo} alt="" width={25} height={25} />
                     </div>
                     <p className="text-center">PineApple - Sistemas</p>
-                    <p className="text-center">&copy; 3S BET LOHAN - 2022 </p>
+                    <p className="text-center">&copy; 3S BET LOHAN - 2022</p>
+                    <div className="icon-rede-social"> 
+                    <p className="text-center"><a target="_blank" href="https://github.com/lohanmattos"><i class="fa-brands fa-github"></i></a></p>
+                    <p className="text-center"><a target="_blank" href="https://www.linkedin.com/in/lohan-amendola-a09b93154/"><i class="fa-brands fa-linkedin"></i></a></p>
+                    </div>
                 </div>
             </footer>
 
