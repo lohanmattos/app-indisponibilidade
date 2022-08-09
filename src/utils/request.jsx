d3a1fda5bf9ccc59dfefbac6f030eebb04c4005f
Lohan Amendola <lohanamendola18@gmail.com>
2022-08-09 15:43:57 -0400

    url da nova api

--- src/utils/request.jsx
+++ src/utils/request.jsx
@@ -3,9 +3,9 @@
 //"http://localhost:3333/afastamentos"
 
 //"https://shrouded-plateau-27488.herokuapp.com/api"
-//"https://api-afastamento.herokuapp.com"
+//https://api-coruja-gestor.herokuapp.com
 //import.meta.env.REACT_APP_BACKEND_URL
 
-let BASE_URL = "https://api-afastamento.herokuapp.com"; 
+let BASE_URL = "https://api-coruja-gestor.herokuapp.com"; 
 
 export default BASE_URL
